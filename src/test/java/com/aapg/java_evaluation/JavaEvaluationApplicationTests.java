031e06e353505ede3dfaf41b8076d7587e142c5c
Alex Painen <alexpainen@gmail.com>
2024-02-11 17:00:05 -0300

    ADD: Initial commit and add springboot proyect

---
+++ src/test/java/com/aapg/java_evaluation/JavaEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aapg.java_evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
